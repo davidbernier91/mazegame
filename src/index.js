143a9b8b10659a95ed29b17cad875ab8507982cc
github username <github email address>
2020-03-05 10:43:38 -0500

    created sass architecture, added google font, creacted react components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '../src/sass/layout/index.scss'
+import App from '../src/componenents/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
