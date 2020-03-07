97df40c90200730521e2fbbb83ea67f247ab1b15
github username <github email address>
2020-03-06 19:33:50 -0500

    added conditional logic to remove walls

--- src/componenents/App.js
+++ src/componenents/App.js
@@ -7,14 +7,16 @@ export default function App() {
 
   const cells = 3
 // const cells = Math.floor(Math.random() * (10 - 5 + 1) + 5)
-  const grid = Array(3).fill(null)
-                        .map(()=> Array(cells).fill(false));
+  const grid = Array(cells)
+                .fill(null)
+                .map(()=> Array(cells).fill(false));
 
-  const verticals = Array(cells).fill(null)
-                                .map(()=>Array(cells - 1 ).fill(false))
+  const verticals = Array(cells)
+                    .fill(null)
+                    .map(()=>Array(cells - 1 ).fill(false))
 
-  const horizonatals = Array(cells-1).fill(null)
-                                     .map(()=>Array(cells).fill(false))
+  const horizontals = Array(cells-1).fill(null)
+                                    .map(()=>Array(cells).fill(false))
 
 
 
@@ -29,7 +31,7 @@ export default function App() {
     while(counter > 0){
       const index = Math.floor(Math.random() * counter)
       // counter--
-      counter-=1
+      counter--
       const temp = arr[counter]
       arr[counter] = arr[index]
       arr[index] = temp
@@ -69,63 +71,53 @@ export default function App() {
 
     const stepThroughCell = (row, column) =>{
       // If you have visited [row, column] return
-
-      // console.log(grid[row][column])
-      // if(grid[row][column]){
-      //   return
-      // }
-
-      grid[row][column] ? grid[row][column] = true : grid[row][column] = false
+      if(grid[row][column]){
+        return
+      }
 
       // // Mark cell as visited
       grid[row][column] = true
       //  Assemble randomly-ordered list of neighbors
-      const up = [row-1, column, 'up']
-      const right = [row, column+1, 'right']
-      const down = [row+1, column, 'down']
-      const left = [row, column-1, 'left']
-      const neighbors = shuffle([up, right, down,left])
+      const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+      ]);
 
       //  For Each neighbor
       for(let neighbor of neighbors){
         const [nextRow, nextColumn, direction] = neighbor
 
       // See if neighbor is out of bounds
-        if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells){
+        if((nextRow < 0) ||
+         (nextRow >= cells) ||
+         (nextColumn < 0) || 
+         (nextColumn >= cells)){
           continue;
         }
       // if you have visited that neighbor, continue to next neighbor
         if(grid[nextRow][nextColumn]){
           continue;
         }
-      // Visit next Cell
-        if(direction === 'left'){
-          verticals[row][column -1] = true
-        }
-        else if(direction === 'right'){
-          verticals[row][column] = true
-        }
-        else if(direction === 'up'){
-          verticals[row - 1][column] = true
-        }
-        else if(direction === 'down'){
-          verticals[row][column] = true
+
+   // Remove a wall from either horizontals or verticals
+        if (direction === 'left'){
+          verticals[row][column - 1] = true;
+        } else if (direction === 'right'){
+          verticals[row][column] = true;
+        } else if (direction === 'up'){
+          horizontals[row - 1][column] = true;
+        } else if (direction === 'down'){
+          horizontals[row][column] = true;
         }
       }
     }
     stepThroughCell(startRow, startColumn)
-
-    // stepThroughCell(1, 1)
-
-
-    // console.log( Bodies)
-    // console.log(horizonatals)
-    // console.log(verticals)
-    // console.log(startColumn)
-    // console.log(startRow)
-    // console.log(startValGen())
-    //     console.log(startColumn)
-    // console.log(startRow)
+    verticals.map((something)=>console.log(something))
+    horizontals.map((something)=>console.log(something))
+    // console.log("verticals", verticals)
+    // console.log("horizontals", horizontals)
   }, [])
 
   return (
