9b559c19cd7612379eec036e2173c0a271992e4d
github username <github email address>
2020-03-08 14:56:50 -0400

    adjusted padding, margins, width and height

--- src/componenents/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NavBar() {
-    return (
-        <div className="nav-bar">
-            <h1 className="nav-bar--title" >Welcome to my maze!</h1>
-        </div>
-    )
-}
